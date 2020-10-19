a7fee3b655269af7b14d890664beb84e182f062f
andiaditya21 <andiaditya21@gmail.com>
2020-10-20 00:15:58 +0800

    index.html, app.js, Header.js

---
+++ src/Component/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron text-center" style={{ marginBottom: 0 }}>
+        <h1>My First Bootstrap 4 Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+      </div>
+    );
+  }
+}
